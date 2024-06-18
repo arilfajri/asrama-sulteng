df87851c6d4eb14851f5487610a8d681946a752d
arilfajri <arielmunchen60@gmail.com>
2024-06-18 21:09:28 +0700

    add environment variable for db

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,8 +1,13 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("db_asramasulteng", "root", "", {
-  host: "localhost",
-  dialect: "mysql",
-});
+const db = new Sequelize(
+  process.env.DB_DBNAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+  }
+);
 
 export default db;
--- backend/index.js
+++ backend/index.js
@@ -40,7 +40,7 @@ app.use(
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:5173",
+    origin: process.env.REACT_URL,
   })
 );
 app.use(express.json());
