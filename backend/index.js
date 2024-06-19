caa4d52e9772c294f7385555c271ed656f6105c9
arilfajri <arielmunchen60@gmail.com>
2024-06-20 03:04:16 +0700

    fix backend 27

--- backend/index.js
+++ backend/index.js
@@ -38,7 +38,7 @@ app.use(
 );
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", process.env.REACT_URL);
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.header("Access-Control-Allow-Credentials", "true");
