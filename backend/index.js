14db303976d0ce7cb345665edd33e48ed6c83d38
arilfajri <arielmunchen60@gmail.com>
2024-06-20 03:20:34 +0700

    fix backend 29

--- backend/index.js
+++ backend/index.js
@@ -38,7 +38,10 @@ app.use(
 );
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", process.env.REACT_URL);
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://asrama-sulteng-p58j.vercel.app"
+  );
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.header("Access-Control-Allow-Credentials", "true");
