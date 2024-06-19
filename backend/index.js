7b5d26df599f25a3b2b6a58ddae74a94ca1919c1
arilfajri <arielmunchen60@gmail.com>
2024-06-20 01:22:58 +0700

    fix backend

--- backend/index.js
+++ backend/index.js
@@ -37,12 +37,13 @@ app.use(
   })
 );
 
-app.use(
-  cors({
-    credentials: true,
-    origin: process.env.REACT_URL,
-  })
-);
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", process.env.REACT_URL);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
 
 app.use(express.json());
 // app.use(express.urlencoded({ extended: false }));
