466adcd1dd93fcbf774988fd9600f4eb09df1516
arilfajri <arielmunchen60@gmail.com>
2024-06-20 03:52:25 +0700

    fix backend 31

--- backend/index.js
+++ backend/index.js
@@ -32,21 +32,19 @@ app.use(
     saveUninitialized: true,
     store: store,
     cookie: {
-      secure: true,
+      secure: "auto",
     },
   })
 );
 
-app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://asrama-sulteng.vercel.app"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Credentials", "true");
-  next();
-});
+app.use(
+  cors({
+    origin: "https://asrama-sulteng.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 // app.use(express.urlencoded({ extended: false }));
