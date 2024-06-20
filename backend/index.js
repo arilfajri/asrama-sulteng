23cd698ec83841520d93b8c5cf1a62a155458cb5
arilfajri <arielmunchen60@gmail.com>
2024-06-20 16:08:52 +0700

    fix backend 41

--- backend/index.js
+++ backend/index.js
@@ -34,7 +34,6 @@ app.use(
     store: store,
     cookie: {
       secure: process.env.NODE_ENV === "production" ? true : "auto",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // Ensure cookies are sent cross-site
     },
   })
 );
@@ -50,7 +49,7 @@ app.use(
 
 app.use(cookieParser());
 app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(fileUpload());
 app.use(express.static("public"));
 app.use(UserRoute);
