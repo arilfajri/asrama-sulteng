989d9060b9638767abe9c019d429ba5e438ee321
arilfajri <arielmunchen60@gmail.com>
2024-07-04 00:25:28 +0700

    fix backend 63

--- backend/index.js
+++ backend/index.js
@@ -32,7 +32,7 @@ app.use(
   session({
     secret: process.env.SESS_SECRET,
     resave: false,
-    saveUninitialized: true,
+    saveUninitialized: false,
     store: store,
     cookie: {
       secure: "auto",
