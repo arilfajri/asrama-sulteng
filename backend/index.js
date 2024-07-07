64fd95cce7dc124e5c069fbf08ab70065e0e2681
arilfajri <arielmunchen60@gmail.com>
2024-07-07 18:15:27 +0700

    fix backend 68

--- backend/index.js
+++ backend/index.js
@@ -27,6 +27,15 @@ const store = new sessionStore({
 })();
 
 app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: "https://asrama-sulteng.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 //
 app.use(
   session({
@@ -40,17 +49,6 @@ app.use(
     },
   })
 );
-
-app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://asrama-sulteng.vercel.app"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Credentials", "true");
-  next();
-});
 
 app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
